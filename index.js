fb57097e0ef7b517207521c83a71e4e259aa811c
vikash93825 <yadav.kk74780@gmail.com>
2021-03-16 17:22:13 +0530

    minor updated

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ function handleForm() {
     var no_of_question = form.get('questions')
     var category = form.get('category');
     var difficulty = form.get('difficulty')
-
     //console.log(difficulty)
-
     window.location = `file:///home/vikash/Masai-School/masai-sprint-3/quiz.html?amount=${no_of_question}&category=${category}&difficulty=${difficulty}`
 }
 
